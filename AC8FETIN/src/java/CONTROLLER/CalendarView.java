d1fc9adc8e91a39e80bc7405179a4d89017278ae
BiaMagri <biancalopes@gec.inatel.br>
2017-11-01 17:30:22 -0200

    Calendar

---
+++ AC8FETIN/src/java/CONTROLLER/CalendarView.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CONTROLLER;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+ 
+@ManagedBean
+public class CalendarView {
+         
+    private Date date10;
+     
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+     
+    public void click() {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+         
+        requestContext.update("form:display");
+        requestContext.execute("PF('dlg').show()");
+    }
+
+    public Date getDate10() {
+        return date10;
+    }
+ 
+    public void setDate10(Date date10) {
+        this.date10 = date10;
+    }
+ 
+
+}
